f5a1848e29415e1b040c8fc13c74219374d16eb4
jimhover <jimhover@163.com>
2021-04-22 00:19:55 +0800

    博客完善

---
+++ jimhover_blog/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Head from './Head';
+import Foot from './Foot';
+
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <h1>hello jimhover</h1>
+      <img src='index.png'></img>
+      <Foot/>
+    </div>
+  );
+}
+
+export default App;
---
+++ jimhover_blog/src/Foot.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Foot extends Component {
+    render() {
+        return (
+            <div>
+                <footer>&copy; Copyright 2021 HTML.am</footer>
+            </div>
+        )
+    }
+}
+
+export default Foot
