c97224f8e3e1e4024ff68c75c2b895406f18227b
jimhover <jimhover@163.com>
2021-04-28 01:48:17 +0800

    增加跳转功能

---
+++ jimhover_blog/src/Head.js
@@ -0,0 +1,24 @@
+import './Head.css'
+import React, { Component } from 'react';
+
+export default function Head(props) {
+    // 'home', 'thought', 'blog', 'ablout'
+    const menus = [{ menu: 'home', path: '/home' }, { menu: 'blog', path: '/blog-list' }];
+    return (g
+        <div className='Head'>
+            {
+                menus.map(item => (
+                    <HeadMenuItem name={item.menu} path={item.path} onClick={props.sayHello} />
+                )
+                )
+            }
+        </div>
+    )
+}
+
+function HeadMenuItem(props) {
+    return (<div className='headMenuItem'>
+        <h2 onClick={()=>{props.onClick(props.path)}}>{props.name}</h2>
+    </div>)
+
+}
